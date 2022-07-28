e875041548adb5cbaca09d8810b494b0ddec422f
Filip Sakrak <g@acts-as-coder.de>
2022-07-28 12:05:02 +0200

    interceptor fixed

--- src/main/kotlin/net/sakrak/demoshoppingcard/config/InterceptorConfig.kt
+++ src/main/kotlin/net/sakrak/demoshoppingcard/config/InterceptorConfig.kt
@@ -1,6 +1,7 @@
 package net.sakrak.demoshoppingcard.config
 
 import net.sakrak.demoshoppingcard.controllers.CustomerLoginInterceptor
+import net.sakrak.demoshoppingcard.controllers.CustomerLoginInterceptorImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
@@ -9,7 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 class InterceptorConfig : WebMvcConfigurerAdapter() {
+    @Autowired
+    lateinit var customerLoginInterceptor: CustomerLoginInterceptor
+
     override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(CustomerLoginInterceptor())
+        registry.addInterceptor(customerLoginInterceptor)
     }
 }
